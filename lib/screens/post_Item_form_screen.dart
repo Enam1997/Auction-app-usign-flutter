e4446abc2356e394ee20c610d46185ed937a3794
Enam1997 <89854906+Enam1997@users.noreply.github.com>
2021-11-05 09:11:55 +0800

    Update post_Item_form_screen.dart

--- lib/screens/post_Item_form_screen.dart
+++ lib/screens/post_Item_form_screen.dart
@@ -3,24 +3,19 @@ import 'package:auctionapp/screens/user_info_screen.dart';
 import 'package:fab_circular_menu/fab_circular_menu.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
 import 'dashbord_screen.dart';
 import 'home_screen.dart';
 
-class PostItemScreen extends StatefulWidget{
-
+class PostItemScreen extends StatefulWidget {
   const PostItemScreen({Key? key, required User user})
       : _user = user,
         super(key: key);
 
   final User _user;
   _PostItemScreenState createState() => _PostItemScreenState();
-
-
-
 }
 
-class _PostItemScreenState extends State<PostItemScreen>{
+class _PostItemScreenState extends State<PostItemScreen> {
   late User _user;
 
   void initState() {
@@ -28,72 +23,82 @@ class _PostItemScreenState extends State<PostItemScreen>{
 
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return SafeArea(child: Scaffold(
+    return SafeArea(
+        child: Scaffold(
       floatingActionButton: FabCircularMenu(
           ringColor: CustomColors.fabMenuRingColor,
           ringWidth: 80,
           fabColor: CustomColors.fabColor,
           fabCloseColor: CustomColors.fabCloseColor,
           children: <Widget>[
-            IconButton(icon: Icon(Icons.home), onPressed: () {
-              Navigator.of(context).pushReplacement(
-                MaterialPageRoute(
-                  builder: (context) => HomeScreen(
-                    user: _user,
-                  ),
-                ),
-              );
-            }),
-            IconButton(icon: Icon(Icons.dashboard), onPressed: () {
-              Navigator.of(context).pushReplacement(
-                MaterialPageRoute(
-                  builder: (context) => DashbordScreen(
-                    user: _user,
-                  ),
-                ),
-              );
-            }),
-            IconButton(icon: Icon(Icons.people), onPressed: () {
-              Navigator.of(context).pushReplacement(
-                MaterialPageRoute(
-                  builder: (context) => UserInfoScreen(
-                    user: _user,
-                  ),
-                ),
-              );
-            }),
-            IconButton(icon: Icon(Icons.add), onPressed: () {
-              Navigator.of(context).pushReplacement(
-                MaterialPageRoute(
-                  builder: (context) => PostItemScreen(
-                    user: _user,
-                  ),
-                ),
-              );
-            })
-
-          ]
-      ),
+            IconButton(
+                icon: Icon(Icons.home),
+                onPressed: () {
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                      builder: (context) => HomeScreen(
+                        user: _user,
+                      ),
+                    ),
+                  );
+                }),
+            IconButton(
+                icon: Icon(Icons.dashboard),
+                onPressed: () {
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                      builder: (context) => DashbordScreen(
+                        user: _user,
+                      ),
+                    ),
+                  );
+                }),
+            IconButton(
+                icon: Icon(Icons.people),
+                onPressed: () {
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                      builder: (context) => UserInfoScreen(
+                        user: _user,
+                      ),
+                    ),
+                  );
+                }),
+            IconButton(
+                icon: Icon(Icons.add),
+                onPressed: () {
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                      builder: (context) => PostItemScreen(
+                        user: _user,
+                      ),
+                    ),
+                  );
+                })
+          ]),
       backgroundColor: CustomColors.appBackGroundColor,
       appBar: AppBar(
         elevation: 0,
         backgroundColor: CustomColors.appBackGroundColor,
         title: Center(
-          child: Text("Auction App",style: TextStyle(
-              color: CustomColors.textColor1.withOpacity(0.8),
-              fontSize: 20,
-              letterSpacing: 0.2)),
+          child: Text("Auction App",
+              style: TextStyle(
+                  color: CustomColors.textColor1.withOpacity(0.8),
+                  fontSize: 20,
+                  letterSpacing: 0.2)),
         ),
       ),
       body: Center(
         child: Text(
-          "Item Form",style: TextStyle(
-          color: CustomColors.textColor1,
-          fontSize: 26,
-        ),
+          "Item Form",
+          style: TextStyle(
+            color: CustomColors.textColor1,
+            fontSize: 26,
+          ),
         ),
       ),
     ));
